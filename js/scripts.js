d881f798a8bc95217d610da37c1f3c85d8db6b53
JEvans890 <evansjexp@gmail.com>
2023-06-15 09:50:11 -0700

    add initial commit and functionality for +1 to a total
    Co-authored-by: Thomas Bakken <tbakken14@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+window.onload = function(event){
+    const h2 = document.querySelector("#Counter");
+    const button = document.querySelector("#button1");
+    button.onclick = function(){
+        console.log(h2.innerText);
+        h2.innerText = 1 + parseInt(h2.innerText);
+    }
+}
+
+function(){
+    const div = document.querySelector
+    //for loop from 0 - 9
+    //  create button
+    //  append button to div
+    //  add event listener to button
+}
+//console.log(h2)
